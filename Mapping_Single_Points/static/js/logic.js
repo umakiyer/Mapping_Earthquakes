536de44446e447ca9688575d2ef6bb7cfbf6ed5e
umakiyer <umaonly@yahoo.com>
2022-03-11 22:00:48 -0500

    Mapping_Circle

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -5,12 +5,20 @@ var myMap = L.map("mapid", {
   zoom: 13
 });
 
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(myMap);
+L.circleMarker([34.0522, -118.2437],{
+  radius:300,
+  color: "black",
+  fillcolor: "#ffffa1"
+}).addTo(myMap);
+
 // Add a tile layer (the background map image) to our map
 // We use the addTo method to add objects to our map
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id: 'mapbox/dark-v10',
     tileSize: 512,
     zoomOffset: -1,
     accessToken: API_KEY
